b232a78c8f9c100cee211c927b0bf0dd3657973f
Note <seeyou85@gmail.com>
2020-09-01 13:14:01 +0300

    Adding room dependencies and initial classes

---
+++ app/src/main/java/stas/batura/radioproject/data/room/RadioDao.kt
@@ -0,0 +1,11 @@
+package ru.batura.stat.batchat.repository.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface RadioDao {
+
+}
