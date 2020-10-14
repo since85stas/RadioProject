d1001559234f1e082d09c49d216070148e929d1d
home <seeyou85@gmail.com>
2020-10-14 14:12:26 +0400

    time in millis add to Podcast.kt

--- app/src/main/java/stas/batura/radioproject/data/dataUtils/TimeUtils.kt
+++ app/src/main/java/stas/batura/radioproject/data/dataUtils/TimeUtils.kt
@@ -1,3 +1,12 @@
 package stas.batura.radioproject.data.dataUtils
 
 val TIME_WEEK = 7*24*60*60*1000L
+
+/**
+ * transform class field [time] to Milliseconds
+ */
+fun getMillisTime(time: String): Long {
+    val dateTime: DateTime = DateTime.parseRfc3339(time)
+    val millis: Long = dateTime.getValue()
+    return millis
+}
